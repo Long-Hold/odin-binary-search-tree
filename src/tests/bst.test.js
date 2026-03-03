542f357b1d4821fb6436011c15afcae25e8e45ba
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-03 16:24:01 -0500

    feat: Import Tree class for testing

---
+++ src/tests/bst.test.js
@@ -0,0 +1,13 @@
+import { Tree } from "../modules/bst";
+
+describe('class Tree', () => {
+    const startingArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+    let tree;
+    beforeEach(() => {
+        tree = new Tree(startingArray);
+    });
+
+    describe('Tree.includes()', () => {
+        
+    });
+});
