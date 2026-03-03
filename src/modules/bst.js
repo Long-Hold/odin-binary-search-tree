ca973c5eff8c476e5d011d57e08585c4572e7581
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-03 15:43:43 -0500

    feat: Add a function to print the BST is a visually comprehensive display

--- src/modules/bst.js
+++ src/modules/bst.js
@@ -37,3 +37,13 @@ export class Tree {
         return arrayToBST(sortedArray, 0, sortedArray.length - 1);
     }
 }
+
+function prettyPrint(node, prefix = '', isLeft = true) {
+    if (node === null || node === undefined) {
+        return;
+    }
+
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
